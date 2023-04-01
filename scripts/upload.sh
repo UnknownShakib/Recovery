ca16cceab4b9d728b06fe6acec1df7f2937b6e57
z3zens <78209717+z3zens@users.noreply.github.com>
2023-04-01 15:39:46 +0700

    Update upload.sh

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -35,13 +35,18 @@ fi
 
 # Upload to WeTransfer
 # NOTE: the current Docker Image, "registry.gitlab.com/sushrut1101/docker:latest", includes the 'transfer' binary by Default
+transfer wet $FILENAME > link.txt || { echo "ERROR: Failed to Upload the Build!" && exit 1; }
+
+# Mirror to oshi.at
 curl -T $FILENAME https://oshi.at/${FILENAME}/${TIMEOUT} > mirror.txt || { echo "WARNING: Failed to Mirror the Build!"; }
 
-DL_LINK=$(cat mirror.txt | grep Download | cut -d\  -f1)
+DL_LINK=$(cat link.txt | grep Download | cut -d\  -f3)
+MIRROR_LINK=$(cat mirror.txt | grep Download | cut -d\  -f1)
 
 # Show the Download Link
 echo "=============================================="
 echo "Download Link: ${DL_LINK}" || { echo "ERROR: Failed to Upload the Build!"; }
+echo "Mirror: ${MIRROR_LINK}" || { echo "WARNING: Failed to Mirror the Build!"; }
 echo "=============================================="
 
 DATE_L=$(date +%d\ %B\ %Y)
