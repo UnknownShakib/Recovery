0d7b658f57dcf0742fb0f5d5c2eced9d33a061fd
z3zens <78209717+z3zens@users.noreply.github.com>
2023-04-01 13:30:37 +0700

    Update upload.sh

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -35,9 +35,9 @@ fi
 
 # Upload to WeTransfer
 # NOTE: the current Docker Image, "registry.gitlab.com/sushrut1101/docker:latest", includes the 'transfer' binary by Default
-transfer wet $FILENAME > link.txt || { echo "ERROR: Failed to Upload the Build!" && exit 1; }
+curl -T $FILENAME https://oshi.at/${FILENAME}/${TIMEOUT} > link.txt || { echo "WARNING: Failed to Mirror the Build!"; }
 
-DL_LINK=$(cat link.txt | grep Download | cut -d\  -f3)
+DL_LINK=$(cat link.txt | grep Download | cut -d\  -f1)
 
 # Show the Download Link
 echo "=============================================="
