3bc20fc52585026ac05daa2b04424200446b8159
UnknownShakib <124501955+UnknownShakib@users.noreply.github.com>
2023-08-14 14:52:50 +0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -13,7 +13,7 @@ export OEM="xiaomi"
 ## "vendorbootimage" - for devices Using vendor boot for the recovery ramdisk (Usually for devices shipped with Android 12 or higher)
 export TARGET="vendorbootimage"
 
-export OUTPUT="OrangeFox*.zip"
+export OUTPUT="OrangeFox-Sky*.zip"
 
 # Additional Dependencies (eg: Kernel Source)
 # Format: "repo dest"
